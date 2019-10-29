ac3bbb48d91c0a574de722017992b8c57ad03c86
MayankMani00 <43845197+MayankMani00@users.noreply.github.com>
2019-10-29 13:37:27 +0530

    Update DS_MINI.C

--- DS_MINI.C
+++ DS_MINI.C
@@ -177,196 +177,198 @@ int main()
     strcpy(option3,"C) 1950s");
     strcpy(option4,"D) 1960s");
     root = create(ques,B,option1,option2,option3,option4,);
-
-    strcpy(ques,"Which of the following european countries did NOT participate in World War 2?");
+	root = insert( root, ptr );
+    
+	strcpy(ques,"Which of the following european countries did NOT participate in World War 2?");
     strcpy(option1,"A) Romania");
     strcpy(option2,"B) Hungary");
     strcpy(option3,"C) Sweden");
     strcpy(option4,"D) Bulgaria");
     root = create(ques,C,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
+	
     strcpy(ques,"In which of the following countries, Google Maps is NOT banned?");
     strcpy(option1,"A) Iran");
     strcpy(option2,"B) North Korea");
     strcpy(option3,"C) Syria");
     strcpy(option4,"D) Ireland");
     root = create(ques,D,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of following Indians was never elected to become India's Prime Minister?");
     strcpy(option1,"A) Atal Bihari Vajpayee");
     strcpy(option2,"B) Rajiv Gandhi");
     strcpy(option3,"C) A.P.J. Abdul Kalam");
     strcpy(option4,"D) Indira Gandhi");
     root = create(ques,C,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Name the CEO of Intel company.");
     strcpy(option1,"A) Bob Swan");
     strcpy(option2,"B) Tim Cook");
     strcpy(option3,"C) Michael Scott");
     strcpy(option4,"D) John Browett");
     root = create(ques,A,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Who discovered oxygen?");
     strcpy(option1,"A) Joseph Priestley");
     strcpy(option2,"B) Gil Amelio");
     strcpy(option3,"C) Michael Spindler");
     strcpy(option4,"D) Jerry York");
     root = create(ques,A,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"In which year was the popular english rock band named "Pink Floyd" formed?");
     strcpy(option1,"A) 1955");
     strcpy(option2,"B) 1965");
     strcpy(option3,"C) 1975");
     strcpy(option4,"D) 1985");
     root = create(ques,B,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"In which year did the popular Indian singer "Arijit Singh" start his music career?");
     strcpy(option1,"A) 2004");
     strcpy(option2,"B) 2007");
     strcpy(option3,"C) 2010");
     strcpy(option4,"D) 2013");
     root = create(ques,B,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"In which decade did the classic music composer named "Ludwig van Beethoven" die?");
     strcpy(option1,"A) 1800s");
     strcpy(option2,"B) 1810s");
     strcpy(option3,"C) 1820s");
     strcpy(option4,"D) 1830s");
     root = create(ques,C,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the following countries have NOT landed humans on moon yet?");
     strcpy(option1,"A) China");
     strcpy(option2,"B) United States");
     strcpy(option3,"C) Russia");
     strcpy(option4,"D) India");
     root = create(ques,D,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the countries has the largest population?");
     strcpy(option1,"A) Bangladesh");
     strcpy(option2,"B) Brazil");
     strcpy(option3,"C) Nigeria");
     strcpy(option4,"D) Russia");
     root = create(ques,B,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the countries has the largest area covered?");
     strcpy(option1,"A) Canada");
     strcpy(option2,"B) United States");
     strcpy(option3,"C) Australia");
     strcpy(option4,"D) India");
     root = create(ques,A,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the countries has the smallest Gross Domestic Product(GDP)?");
     strcpy(option1,"A) India");
     strcpy(option2,"B) China");
     strcpy(option3,"C) United Kingdom");
     strcpy(option4,"D) Japan");
     root = create(ques,C,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the following countries is the most "Biodiversed"?");
     strcpy(option1,"A) China");
     strcpy(option2,"B) Brazil");
     strcpy(option3,"C) South Africa");
     strcpy(option4,"D) Thailand");
     root = create(ques,B,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the countries is the most densely populated?");
     strcpy(option1,"A) Singapore");
     strcpy(option2,"B) Bangladesh");
     strcpy(option3,"C) Vatican City");
     strcpy(option4,"D) Hong Kong");
     root = create(ques,A,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the following languages is the most spoken?");
     strcpy(option1,"A) Spanish");
     strcpy(option2,"B) Russian");
     strcpy(option3,"C) Arabic");
     strcpy(option4,"D) Mandarin");
     root = create(ques,D,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the following countries is the the most advanced in terms of technology?");
     strcpy(option1,"A) Canada");
     strcpy(option2,"B) India");
     strcpy(option3,"C) Germany");
     strcpy(option4,"D) Finland");
     root = create(ques,C,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the following animals is the largest in size?");
     strcpy(option1,"A) Colossal Squid");
     strcpy(option2,"B) Giraffe");
     strcpy(option3,"C) African Elephant");
     strcpy(option4,"D) Brown Bear");
     root = create(ques,A,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the following planets is the biggest in size?");
     strcpy(option1,"A) Uranus");
     strcpy(option2,"B) Neptune");
     strcpy(option3,"C) Saturn");
     strcpy(option4,"D) Earth");
     root = create(ques,C,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Who invented telephone?");
     strcpy(option1,"A) Joseph Swan ");
     strcpy(option2,"B) Alexander Graham Bell");
     strcpy(option3,"C) Hans Lippershey");
     strcpy(option4,"D) Jacob Metius");
     root = create(ques,B,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Who invented telescope?");
     strcpy(option1,"A) Joseph Swan ");
     strcpy(option2,"B) Alexander Graham Bell");
     strcpy(option3,"C) Hans Lippershey");
     strcpy(option4,"D) Jacob Metius");
     root = create(ques,C,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Who invented Computer?");
     strcpy(option1,"A) Joseph Swan");
     strcpy(option2,"B) Zacharias Janssen");
     strcpy(option3,"C) Robert Hooke");
     strcpy(option4,"D) Charles Babbage");
     root = create(ques,D,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Who invented Dynamite?");
     strcpy(option1,"A) Alfred Nobel");
     strcpy(option2,"B) Marie Curie");
     strcpy(option3,"C) Thomas Edison");
     strcpy(option4,"D) Albert Einstein");
     root = create(ques,A,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the following cities in India is the most polluted?");
     strcpy(option1,"A) Delhi");
     strcpy(option2,"B) Kanpur");
     strcpy(option3,"C) Faridabad");
     strcpy(option4,"D) Lucknow");
     root = create(ques,B,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the cities is the most expensive in the world?");
     strcpy(option1,"A) Los Angeles");
     strcpy(option2,"B) New York");
     strcpy(option3,"C) Osaka");
     strcpy(option4,"D) Singapore");
     root = create(ques,D,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the countries is the most educated in the world?");
     strcpy(option1,"A) Korea");
     strcpy(option2,"B) United States");
     strcpy(option3,"C) Canada");
     strcpy(option4,"D) Australia");
     root = create(ques,C,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the following animal species is the deadliest to humans?");
     strcpy(option1,"A) Snakes");
     strcpy(option2,"B) Tapeworms");
     strcpy(option3,"C) Humans themselves");
     strcpy(option4,"D) Assassin bugs");
     root = create(ques,C,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
     strcpy(ques,"Which one of the following religions in India is the LEAST populated?");
     strcpy(option1,"A) Sikhs");
     strcpy(option2,"B) Jains");
     strcpy(option3,"C) Christians");
     strcpy(option4,"D) Hindus");
     root = create(ques,B,option1,option2,option3,option4,);
-
+root = insert( root, ptr );
 
 	ch = instructions();
 
